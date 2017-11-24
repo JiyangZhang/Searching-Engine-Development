6bb31faae83f552175be3c091b61f84f10d79da0
JiyangZhang <33189384+JiyangZhang@users.noreply.github.com>
2017-11-24 08:52:09 -0500

    Update termination.py

--- lab4/termination.py
+++ lab4/termination.py
@@ -1,8 +1,7 @@
 import  boto.ec2
 import time
 
-conn = boto.ec2.connect_to_region('us-east-1', aws_access_key_id = 'AKIAJHGLFCEL3SCBBSAA', aws_secret_access_key = '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\
-')
+conn = boto.ec2.connect_to_region('us-east-1', aws_access_key_id = '#', aws_secret_access_key = '#\')
 my_id = raw_input("input the instance ID:")
 lst = conn.terminate_instances(instance_ids=[my_id])
 for i in lst:
