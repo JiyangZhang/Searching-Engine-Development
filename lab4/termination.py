3daaaebba525b69e107e7f77562b86f05b5ac2f9
JiyangZhang <33189384+JiyangZhang@users.noreply.github.com>
2017-12-03 21:10:19 -0500

    Add files via upload

--- lab4/termination.py
+++ lab4/termination.py
@@ -1,8 +1,20 @@
 import  boto.ec2
-import time
+import sys
+#get access
+f = open('config.txt')    # people have to type their key_id at the first line and secret at the second line
+l = []
+for i in f:
+    if i[-1] == '\n':
+        l.append(i[:-2])
+    else:
+        l.append(i)
+f.close()
+# get the key
+key_id = l[0]
+secret_access_key = l[1]
 
-conn = boto.ec2.connect_to_region('us-east-1', aws_access_key_id = '#', aws_secret_access_key = '#\')
-my_id = raw_input("input the instance ID:")
-lst = conn.terminate_instances(instance_ids=[my_id])
+conn = boto.ec2.connect_to_region('us-east-1', aws_access_key_id = str(key_id), aws_secret_access_key = str(secret_access_key))
+instance_id = raw_input("Please enter instance_id: ")  # Python 2
+lst = conn.terminate_instances(instance_ids=[instance_id])
 for i in lst:
     print("%s has been terminated" % str(i))
